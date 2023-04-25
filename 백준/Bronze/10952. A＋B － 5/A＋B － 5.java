5705e97aa9a9270a9a36808a0fc25a83ac576af0
lim jeong han <93763627+jeoung-han@users.noreply.github.com>
2023-04-25 10:09:34 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.lines().forEach(s -> {
+			String[] arr = s.split(" ");
+			int sum = Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]);
+			if(sum!=0) System.out.println(sum);
+		});
+	}
+}
